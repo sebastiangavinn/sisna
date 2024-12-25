800875972ce1aa6463971b781609470572ea3980
sebastiangavinn <sebastiangavin80@gmail.com>
2024-12-25 18:50:24 +0700

    test commit

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
-  )
-}
+    <div className="bg-white w-full rounded-lg">
+      <div className="px-6 py-8">
+        <h1>Detail Profile</h1>
+      </div>
+    </div>
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
